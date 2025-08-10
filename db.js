d949b1930672aca0ac5b4f281fa66db65f2983af
Karina Navarro <ns4karina@gmail.com>
2025-08-10 15:57:11 -0600

    fix(db): accept self-signed cert for Railway public MySQL

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,        // en Railway usaremos el host interno
-  port: process.env.DB_PORT,        // 3306 si es host interno
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : undefined,
+  // 👇 si DB_SSL=true, no rechazar self-signed
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
   waitForConnections: true,
   connectionLimit: 5,
-  multipleStatements: true           // importante para usar OUT vars
+  multipleStatements: true
 });
